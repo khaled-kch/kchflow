170223050dae2b675553e793964cc3a7175fa5c4
kchayeb <kchayeb>
2024-03-17 11:43:34 +0100

    renommer namespaces et supprimer svelte

--- packages/svelte/src/lib/components/KeyHandler/types.ts
+++
@@ -1,9 +0,0 @@
-import type { KeyDefinition } from '$lib/types';
-
-export type KeyHandlerProps = {
-  selectionKey?: KeyDefinition | null;
-  multiSelectionKey?: KeyDefinition | null;
-  deleteKey?: KeyDefinition | null;
-  panActivationKey?: KeyDefinition | null;
-  zoomActivationKey?: KeyDefinition | null;
-};
--- packages/svelte/src/lib/components/SvelteFlowProvider/types.ts
+++
@@ -1,9 +0,0 @@
-import type { Edge, Node } from '$lib/types';
-
-export type SvelteFlowProviderProps = {
-  initialNodes?: Node[];
-  initialEdges?: Edge[];
-  initialWidth?: number;
-  initialHeight?: number;
-  fitView?: boolean;
-};
